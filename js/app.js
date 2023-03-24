b42931065322b134d262876071522071edeade63
ValeriaM <valemolinac05@gmail.com>
2023-03-23 19:12:57 -0500

    proyecto con validación

--- js/app.js
+++ js/app.js
@@ -45,6 +45,26 @@ const cerrarMenu= (boton,overlay)=>{
     }
 
 }
+// Validación formulario
+const formulario = document.querySelector('.formulario');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const correo = document.getElementById('correo');
+const telefono = document.getElementById('telefono');
+const mensaje = document.querySelector('textarea');
 
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  if (nombre.value === '' || apellido.value === '' || correo.value === '' || telefono.value === '' || mensaje.value === '') {
+    alert('Por favor, completa todos los campos.');
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(correo.value)) {
+    alert('Por favor, introduce una dirección de correo válida.');
+  } else if (!/^\+?\d{1,3}[- ]?\d{3}[- ]?\d{4}$/.test(telefono.value)) {
+    alert('Por favor, introduce un número de teléfono válido.');
+  } else {
+    alert('Formulario enviado correctamente.');
+  }
+});
 
 
